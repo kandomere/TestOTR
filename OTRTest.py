3f81c61cfc87bb8063c3d0930a3eb28cfcbadfa0
kandomere <assasin21rus@gmail.com>
2021-01-16 23:06:55 +0300

    все работает но сообдения толкают кнопки

--- OTRTest.py
+++ OTRTest.py
@@ -30,16 +30,20 @@ def sum_month(sum_year, sum_month):
         return 'Отсуствуют платежи\n за выбранный месяц'
 
 
-def res_add(res_name, res_surname, res_city, res_email):
+def res_add(res_name, res_surname, inn_client, res_city, res_email):
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if any(map(str.isdigit, res_name)) or len(res_name) <= 3:
         return 'Введите корректоное имя'
     if any(map(str.isdigit, res_surname)) or len(res_surname) <= 3:
         return 'Введите фамилию'
+    if not any(map(str.isdigit, inn_client)):
+        return 'Введите ИНН\n из 12 цифр'
+    elif len(inn_client) < 12:
+        return 'Введите ИНН\n из 12 цифр'
     if any(map(str.isdigit, res_city)) or len(res_city) <= 3:
         return 'Введите город'
     if re.search(regex, res_email):
-        SQLfunks.create_client(res_name, res_surname, res_city, res_email)
+        SQLfunks.create_client(res_name, res_surname,inn_client, res_city, res_email)
         clean_client()
         return 'добавлен в бд'
     else:
@@ -84,6 +88,8 @@ def clean_client():
     cityField.insert(0, "")
     emailField.delete(0, END)
     emailField.insert(0, "")
+    inn_clientField.delete(0, END)
+    inn_clientField.insert(0, "")
 
 
 def clean_trans():
@@ -95,12 +101,59 @@ def clean_trans():
     sum_trans.insert(0, "")
 
 
+def clean_invoice():
+    invoiceField.delete(0, END)
+    invoiceField.insert(0, '')
+    bank_nameField.delete(0, END)
+    bank_nameField.insert(0, "")
+    cityInvoiceField.delete(0, END)
+    cityInvoiceField.insert(0, '')
+
+    bikField.delete(0, END)
+    bikField.insert(0, '')
+    innclientField.delete(0, END)
+    innclientField.insert(0, '')
+
+
 def add_client():
     add_name = nameField.get()
     add_surname = surnameField.get()
     add_city = cityField.get()
     add_email = emailField.get()
-    added['text'] = res_add(add_name, add_surname, add_city, add_email)
+    inn_client = inn_clientField.get()
+    added['text'] = res_add(add_name, add_surname, inn_client, add_city, add_email)
+
+
+def res_add_invoice(invoice, bank_name, city_invoice,innclient, bik):
+    pass
+    if not any(map(str.isdigit, invoice)):
+        return 'Введите счет\n из 20 цифр'
+    if len(invoice) != 20:
+        return 'Введите корректоный счет\n из 20 символов'
+    if len(bank_name) <= 0:
+        return 'Введите банк'
+    if any(map(str.isdigit, city_invoice)) or len(city_invoice) <= 3:
+        return 'Введите город'
+    if not any(map(str.isdigit, innclient)):
+        return 'Введите ИНН\n из 12 цифр'
+    if len(innclient) != 12:
+        return 'Введите ИНН\n из 12 цифр'
+    if not any(map(str.isdigit, bik)):
+        return 'Введите Бик\n из 9 цифр'
+    if len(bik) != 9:
+        return 'Введите Бик\n из 9 цифр'
+    else:
+        SQLfunks.create_invoice(invoice, bank_name, city_invoice, innclient, bik)
+        clean_invoice()
+
+
+def add_invoice():
+    invoice = invoiceField.get()
+    bank_name = bank_nameField.get()
+    city_invoice = cityInvoiceField.get()
+    bik = bikField.get()
+    innclient = innclientField.get()
+    added_invoice['text'] = res_add_invoice(invoice, bank_name, city_invoice,innclient,bik)
 
 
 def show_clients():
@@ -108,7 +161,7 @@ def show_clients():
 
 
 def show_invoice():
-    SQLfunks.show_invoice()
+    SQLfunks.show_invoices()
 
 
 def show_transfer():
@@ -133,81 +186,103 @@ root.resizable(width=False, height=False)
 # Для суммы за месяц
 year = Label(text='Введите год', font=40)
 year.grid(row=0, column=0, sticky=W)
-
 yearField = Entry(bg='white', font=20)
 yearField.grid(row=0, column=1, padx=10)
-
 month = Label(text='Введите месяц', font=40)
 month.grid(row=1, column=0, sticky=W)
-
 monthField = Entry(bg='white', font=30)
 monthField.grid(row=1, column=1)
-
 btn = Button(text='Вычислить сумму', command=get_sum)
 btn.grid(row=2, column=0, sticky=W, pady=5)
+info = Label(text='Сумма за месяц', font=20)
+info.grid(row=2, column=1, sticky=E + W,rowspan=2)
 
-info = Label(text='Сумма за месяц', font=40)
-info.grid(row=2, column=1, sticky=E + W)
-
+# Секция доабвления клиента
 name = Label(text='Введите имя', font=40)
 name.grid(row=6, column=0, sticky=W)
-
 nameField = Entry(bg='white', font=20)
 nameField.grid(row=6, column=1, padx=10)
-
 surname = Label(text='Введите фамилию', font=40)
 surname.grid(row=7, column=0, sticky=W)
-
 surnameField = Entry(bg='white', font=30)
 surnameField.grid(row=7, column=1)
 
+inn_clientField = Label(text='Введите ИНН', font=40)
+inn_clientField.grid(row=8, column=0, sticky=W)
+inn_clientField = Entry(bg='white', font=30)
+inn_clientField.grid(row=8, column=1)
+
 city = Label(text='Введите город', font=40)
-city.grid(row=8, column=0, sticky=W)
-
+city.grid(row=9, column=0, sticky=W)
 cityField = Entry(bg='white', font=30)
-cityField.grid(row=8, column=1)
-
+cityField.grid(row=9, column=1)
 email = Label(text='Введите почту', font=40)
-email.grid(row=9, column=0, sticky=W)
-
+email.grid(row=10, column=0, sticky=W)
 emailField = Entry(bg='white', font=30)
-emailField.grid(row=9, column=1)
-
+emailField.grid(row=10, column=1)
 btn_add_client = Button(text='Добавить клиента', command=add_client)
-btn_add_client.grid(row=10, column=0, sticky=W, pady=5)
-
+btn_add_client.grid(row=11, column=0, sticky=W, pady=5)
 added = Label(text='Заполните поля', font=40)
-added.grid(row=10, column=1, pady=5)
+added.grid(row=11, column=1, pady=5)
 
+# Секция добавления перевода
 deb = Label(text='счет-дебет', font=40)
-deb.grid(row=11, column=0, sticky=W)
-
+deb.grid(row=12, column=0, sticky=W)
 deb = Entry(bg='white', font=20)
-deb.grid(row=11, column=1, padx=10)
-
+deb.grid(row=12, column=1, padx=10)
 credit = Label(text='счет-кредит', font=40)
-credit.grid(row=12, column=0, sticky=W)
-
+credit.grid(row=13, column=0, sticky=W)
 credit = Entry(bg='white', font=30)
-credit.grid(row=12, column=1)
-
+credit.grid(row=13, column=1)
 sum_trans = Label(text='сумма проводки', font=40)
-sum_trans.grid(row=13, column=0, sticky=W)
-
+sum_trans.grid(row=14, column=0, sticky=W)
 sum_trans = Entry(bg='white', font=30)
-sum_trans.grid(row=13, column=1)
-
+sum_trans.grid(row=14, column=1)
 add_trans = Button(text='Добавить перевод', command=add_trans)
-add_trans.grid(row=14, column=0, sticky=W, pady=5)
-
+add_trans.grid(row=15, column=0, sticky=W, pady=5)
 added_trans = Label(text='Заполните поля', font=40)
-added_trans.grid(row=14, column=1, pady=5)
+added_trans.grid(row=15, column=1, pady=5)
 
+# Секция доавбления счета
+
+invoiceField = Label(text='счет', font=40)
+invoiceField.grid(row=16, column=0, sticky=W)
+invoiceField = Entry(bg='white', font=20)
+invoiceField.grid(row=16, column=1, padx=10)
+
+bank_nameField = Label(text='Банк', font=40)
+bank_nameField.grid(row=17, column=0, sticky=W)
+bank_nameField = Entry(bg='white', font=30)
+bank_nameField.grid(row=17, column=1)
+
+cityInvoiceField = Label(text='Город', font=40)
+cityInvoiceField.grid(row=18, column=0, sticky=W)
+cityInvoiceField = Entry(bg='white', font=30)
+cityInvoiceField.grid(row=18, column=1)
+
+innclientField = Label(text='ИНН клиента', font=40)
+innclientField.grid(row=19, column=0, sticky=W)
+innclientField = Entry(bg='white', font=30)
+innclientField.grid(row=19, column=1)
+
+bikField = Label(text='Бик', font=40)
+bikField.grid(row=20, column=0, sticky=W)
+bikField = Entry(bg='white', font=30)
+bikField.grid(row=20, column=1)
+
+
+
+inn_btn = Button(text='Добавить счет', command=add_invoice)
+inn_btn.grid(row=21, column=0, sticky=W, pady=5)
+added_invoice = Label(text='Заполните поля', font=40)
+added_invoice.grid(row=21, column=1, pady=5)
+
+# Секция вывода таблиц
 show_clients = Button(text='Посмотреть таблицу\nклиентов', command=show_clients)
-show_clients.grid(row=15, column=0, sticky=W, pady=5)
-# show_clients = Button(text='Посмотреть таблицу\nсчетов', command=show_invoice)
-# show_clients.grid(row=15, column=1, sticky=W, pady=5)
+show_clients.grid(row=22, column=0, sticky=W+E, pady=5)
+show_clients = Button(text='Посмотреть таблицу\nсчетов', command=show_invoice)
+show_clients.grid(row=22, column=1, sticky=W+E, pady=5)
 show_clients = Button(text='Посмотреть таблицу\nтранзакций', command=show_transfer)
-show_clients.grid(row=15, column=2, sticky=W, pady=1)
+show_clients.grid(row=22, column=2, sticky=W+E, pady=1)
 SQLfunks.create_databases()
 root.mainloop()
--- SQLfunks.py
+++ SQLfunks.py
@@ -5,23 +5,24 @@ import sqlite3
 import tkinter as tk
 import tkinter.ttk as ttk
 
+
 def create_databases():
     """Создание трех таблиц"""
     with sql.connect('Test.db') as db:
         cur = db.cursor()
         cur.execute("""CREATE TABLE IF NOT EXISTS clients (client_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR(30),surname VARCHAR(30),city VARCHAR(45), email VARCHAR(50))""")
+                    name VARCHAR(30),surname VARCHAR(30),INN VARCHAR(12),city VARCHAR(45), email VARCHAR(50))""")
 
         cur.execute("""CREATE TABLE IF NOT EXISTS invoice  (invoice_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    invoice INTEGER(20),bank_name VARCHAR(225), city VARCHAR(45), INN INTEGER(12), BIK INTEGER(9),
-                    CUSTOMER_ID INT references clients(client_id))""")
+                    invoice VARCHAR(20),bank_name VARCHAR(225), city VARCHAR(45), BIK VARCHAR(9),
+                    CUSTOMER_ID VARCHAR references clients(client_inn))""")
 
         cur.execute("""CREATE TABLE IF NOT EXISTS transfer (transfer_id INTEGER PRIMARY KEY AUTOINCREMENT,
         date_formation DATETIME  NOT NULL , debit_account VARCHAR(20),credit_account VARCHAR(20), 
                       total_transfer FLOAT,year_month TEXT)""")
 
 
-def create_transfer(debit_account,credit_account,total_transfer):
+def create_transfer(debit_account, credit_account, total_transfer):
     """добавлние значений в Проводок"""
     date = datetime.datetime.now().strftime("%Y-%m-%d")
     year_month = datetime.datetime.now().strftime("%Y_%m")
@@ -33,22 +34,22 @@ def create_transfer(debit_account,credit_account,total_transfer):
     db.commit()
 
 
-def create_client(name, surname, city, email):
+def create_client(name, surname,inn, city, email):
     """Создание клиента"""
     with sql.connect('Test.db') as db:
         cur = db.cursor()
-        cur.execute("INSERT INTO clients (client_id,name, surname, city,email) VALUES (null,?,?,?,?)",
-                    (name, surname, city, email))
+        cur.execute("INSERT INTO clients (client_id,name, surname,inn, city,email) VALUES (null,?,?,?,?,?)",
+                    (name, surname,inn, city, email))
     db.commit()
 
 
-def create_invoice(invoice, bank_name, city, inn, bik):
+def create_invoice(invoice, bank_name, city,inn_client,bik,):
     """Добавление счета"""
     with sql.connect('Test.db') as db:
         cur = db.cursor()
-        cur.execute("INSERT INTO clients (invoice_id,invoice, bank_name, city, inn,bik,CUSTOMER_ID)"
-                    " VALUES (null,?,?,?,?,?,?)",
-                    (invoice, bank_name, city, inn, bik))
+        cur.execute("INSERT INTO invoice (invoice_id,invoice, bank_name, city,CUSTOMER_ID,bik)"
+                    " VALUES (null,?,?,?,?,?)",
+                    (invoice, bank_name, city,inn_client,bik))
     db.commit()
 
 
@@ -80,10 +81,11 @@ def show_tables():
         data = (row for row in cursor.fetchall())
 
     root = tk.Tk()
-    table = Table(root, headings=('№', 'Имя', 'Фамилия', 'Город', 'Почта'), rows=data)
+    table = Table(root, headings=('№', 'Имя', 'Фамилия', 'ИНН', 'Город','Почта'), rows=data)
     table.pack(expand=tk.YES, fill=tk.BOTH)
 
-def show_invoice():
+
+def show_invoices():
     class Table(tk.Frame):
         def __init__(self, parent=None, headings=tuple(), rows=tuple()):
             super().__init__(parent)
@@ -115,37 +117,6 @@ def show_invoice():
     table.pack(expand=tk.YES, fill=tk.BOTH)
 
 
-def show_invoices():
-    class Table(tk.Frame):
-        def __init__(self, parent=None, headings=tuple(), rows=tuple()):
-            super().__init__(parent)
-
-            table = ttk.Treeview(self, show="headings", selectmode="browse")
-            table["columns"] = headings
-            table["displaycolumns"] = headings
-
-            for head in headings:
-                table.heading(head, text=head, anchor=tk.CENTER)
-                table.column(head, anchor=tk.CENTER)
-
-            for row in rows:
-                table.insert('', tk.END, values=tuple(row))
-
-            scrolltable = tk.Scrollbar(self, command=table.yview)
-            table.configure(yscrollcommand=scrolltable.set)
-            scrolltable.pack(side=tk.RIGHT, fill=tk.Y)
-            table.pack(expand=tk.YES, fill=tk.BOTH)
-
-    data = (",")
-    with sqlite3.connect('test.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM transfer")
-        data = (row for row in cursor.fetchall())
-
-    root = tk.Tk()
-    table = Table(root, headings=('№', 'Счет', 'Банк', 'Город', 'ИНН', 'Бик'), rows=data)
-    table.pack(expand=tk.YES, fill=tk.BOTH)
-
 def show_transfers():
     class Table(tk.Frame):
         def __init__(self, parent=None, headings=tuple(), rows=tuple()):
@@ -174,5 +145,5 @@ def show_transfers():
         data = (row for row in cursor.fetchall())
 
     root = tk.Tk()
-    table = Table(root, headings=('№', 'Дата','Счет-дебит', 'Счет-кредит', 'Сумма'), rows=data)
+    table = Table(root, headings=('№', 'Дата', 'Счет-дебит', 'Счет-кредит', 'Сумма'), rows=data)
     table.pack(expand=tk.YES, fill=tk.BOTH)
